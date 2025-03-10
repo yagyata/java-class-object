b9ca13ffaaf1aaec3d99c4ea6c2eca9fbe7465f0
Yagyata <yagyatasharmaa@gmail.com>
2025-03-10 14:58:02 +0530

    Calculating and displaying student details

---
+++ Student.java
@@ -0,0 +1,39 @@
+public class Student {
+    // Attributes
+    private String name;
+    private int rollNumber;
+    private double marks;
+
+    // Constructor
+    public Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    public char calculateGrade() {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 80) {
+            return 'B';
+        } else if (marks >= 70) {
+            return 'C';
+        } else if (marks >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    public void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Yagyata", 255, 85.5);
+        student1.displayDetails();
+    }
+}
