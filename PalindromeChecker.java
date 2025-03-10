02bbd07ad206b04f813ef2e4335815c11214f567
Yagyata <yagyatasharmaa@gmail.com>
2025-03-10 15:15:36 +0530

    Check whether a string is a palindrome

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class PalindromeChecker {
+public class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String cleanedText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); // Remove spaces & special characters
+        int length = cleanedText.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (cleanedText.charAt(i) != cleanedText.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker checker1 = new PalindromeChecker("racecar");
+        checker1.displayResult(); // Output: "racecar" is a palindrome.
+
+        PalindromeChecker checker2 = new PalindromeChecker("hello");
+        checker2.displayResult(); // Output: "hello"
+    }
 }
