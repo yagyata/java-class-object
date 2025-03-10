3665adcd43fd870344c5d1c40d00869d4bdb9f9b
Yagyata <yagyatasharmaa@gmail.com>
2025-03-10 14:36:51 +0530

    Creating restaurant objects

--- Restaurant.java
+++ Restaurant.java
@@ -29,4 +29,27 @@ public class Restaurant {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        //Define food items for restaurants
+        String[] foodItems1 = {"Pizza", "Pasta", "Burger"};
+        String[] foodItems2 = {"Sushi", "Ramen", "Tempura"};
+
+        //Create Restaurant objects
+        Restaurant restaurant1 = new Restaurant("Italian Delight", "Downtown", foodItems1);
+        Restaurant restaurant2 = new Restaurant("Tokyo Treats", "Uptown", foodItems2);
+
+        //Display details of each restaurant
+        System.out.println("***Restaurant 1***");
+        restaurant1.displayDetails();
+        System.out.println("***Restaurant 1***");
+        restaurant2.displayDetails();
+
+        //Check food availability
+        System.out.println("\nChecking Food Availability");
+        System.out.println("Is Pasta available in Italian Delight? "+ restaurant1
+                .isFoodAvailable("Pasta"));
+        System.out.println("Is Sushi available in Italian Delight? " + restaurant1.isFoodAvailable("Sushi"));
+
+    }
 }
